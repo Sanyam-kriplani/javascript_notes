938cd2ede268581569a3ab62fee2548260b0d4a2
Sanyam kriplani <144801752+Sanyam-kriplani@users.noreply.github.com>
2024-08-08 18:04:37 +0000

    data type conversions

---
+++ basics_101/03_conversionOperations.js
@@ -0,0 +1,35 @@
+// let numb="3";
+// console.log(typeof numb);//return string
+
+// let num=Number (numb);//Capital N to remember convert any dt to number
+// console.log(typeof num);//return number
+// console.log(num);
+
+// let numb2="3a";
+// console.log(typeof numb2);//return string
+
+// let num2=Number (numb2);//Capital N to remember convert any dt to number
+// console.log(typeof num2);//return number
+// console.log(num2);//return NaN means not a number but its type is actually a number
+
+//"3"=>3 but "3a"=>NaN
+
+// let a="abc";
+// console.log(typeof a);
+// let b=Boolean(a);
+// console.log(typeof b);
+// console.log(b);
+
+//1=>true and 0=>false
+//""=>false and "abc"=>true
+
+// let sumNum=45;
+// console.log(typeof sumNum);
+
+// let sumString=String(sumNum);
+// console.log(typeof sumString);
+// console.log(sumString);
+
+//
+
+
