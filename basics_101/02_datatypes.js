65db64a293384d01e81dcda79edbbcd685fe7979
Sanyam kriplani <144801752+Sanyam-kriplani@users.noreply.github.com>
2024-08-08 14:51:59 +0000

    datatypes in js

---
+++ basics_101/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//this statement states that all the code will be according to the newer version of the language.
+let name="sanyam";//string 
+let age=20;// integer
+let male=true;//boolean
+
+//number=> range till 2^1024
+//string=> "" or ''
+//boolean=>true/false
+//null=>it is a standalone value that is used represent the empty value
+//undefined=> it is used to represent that the value to the variable is not defined yet.
+//symbol=>is used to represent the uniqueness mostly used in react
+//object=>we will learn about this later
+
+console.log(typeof null);//will return object but why??
+console.log(typeof undefined);//will return undefined.
+
